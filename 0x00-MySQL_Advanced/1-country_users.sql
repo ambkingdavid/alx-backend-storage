2cbfed9bf0f67d966bb6b233e248e767e07bc878
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-09 18:36:51 +0100

    update

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,11 +1,11 @@
---script that creates a table users
+-- script that creates a table users
 -- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 -- country, enumeration of countries: US, CO and TN, never null 
---(= default will be the first element of the enumeration, here US)
+-- (= default will be the first element of the enumeration, here US)
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country VARCHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
