d75b98eb6419a4d458e49e6db909cff8d83ebcc1
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-09 18:24:25 +0100

    fix typo

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create table if not exist
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     email TEXT NOT NULL UNIQUE,
     name TEXT
 );
