518556d6a8b55ed72ed4a014e2a1e69adbb2754c
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-15 13:03:08 +0100

    add task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""12-log_stats"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    logs_collection = client.logs.nginx
+
+    count_logs = logs_collection.count_documents({})
+    print(f"{count_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count_method = logs_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count_method}")
+
+    count_path = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{count_path} status check")
