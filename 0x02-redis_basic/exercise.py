d45e2d8548841ea5d999f9658eeff46386ef0d3e
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-16 17:28:23 +0100

    edit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -48,8 +48,9 @@ def count_calls(fn: Callable) -> Callable:
         """
         increment count
         """
-        key = fn.__qualname__
-        self._redis.incr(key)
+        if isinstance(self._radis, redis.Redis):
+            key = fn.__qualname__
+            self._redis.incr(key)
         return fn(self, *args, **kwargs)
     return wrapper
 
