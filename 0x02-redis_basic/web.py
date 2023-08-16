d1ee06604c0f7c7de11f1b3dd14cf549f9dd70f5
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-16 16:46:17 +0100

    edit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,62 +3,44 @@
 """Contains a web class"""
 
 import requests
-import redis
 from functools import wraps
+from datetime import datetime, timedelta
+from typing import Callable, Any, Dict
 
+cache = {}
 
-class WebCache:
-    def __init__(self):
-        self._redis: redis.Redis = redis.Redis()
-
-    def count_url(self, url: str) -> str:
-        return f"count:{url}"
-
-    def cache_key(self, url: str) -> str:
-        return f"cache:{url}"
-
-
-def count_access(method):
+def cache_decorator(expiration_time: int) -> Callable:
     """
-    Decorator to count the number of times a URL is accessed.
+    A decorator function that caches the results of a function for a specified amount of time.
+    
+    :param expiration_time: The amount of time in seconds to cache the results of the function.
+    :return: A decorator that can be used to cache the results of a function.
     """
-    @wraps(method)
-    def wrapper(self, url: str, *args, **kwargs) -> str:
-        count_key: str = self.cache.count_url(url)
-        self._redis.incr(count_key)
-        return method(self, url, *args, **kwargs)
-
-    return wrapper
-
-
-def cache_result(expiration: int = 10):
-    """
-    Decorator to cache function results with an expiration time.
-    """
-    def decorator(method):
-        @wraps(method)
-        def wrapper(self, url: str, *args, **kwargs) -> str:
-            cache_key: str = self.cache.cache_key(url)
-            cached_result: bytes = self._redis.get(cache_key)
-            if cached_result:
-                return cached_result.decode("utf-8")
-
-            result: str = method(self, url, *args, **kwargs)
-            self._redis.setex(cache_key, expiration, result)
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            url = args[0]
+            now = datetime.now()
+            if url in cache and cache[url]['expiration_time'] > now:
+                cache[url]['count'] += 1
+                return cache[url]['result']
+            result = func(*args, **kwargs)
+            cache[url] = {
+                'result': result,
+                'expiration_time': now + timedelta(seconds=expiration_time),
+                'count': 1
+            }
             return result
-
         return wrapper
-
     return decorator
 
-
-class Web:
-    def __init__(self):
-        self._redis: redis.Redis = redis.Redis()
-        self.cache: WebCache = WebCache()
-
-    @count_access
-    @cache_result()
-    def get_page(self, url: str) -> str:
-        response: requests.Response = requests.get(url)
-        return response.text
+@cache_decorator(expiration_time=10)
+def get_page(url: str) -> str:
+    """
+    A function that uses the requests module to obtain the HTML content of a particular URL and returns it.
+    
+    :param url: The URL to get the HTML content from.
+    :return: The HTML content of the specified URL.
+    """
+    response = requests.get(url)
+    return response.text
